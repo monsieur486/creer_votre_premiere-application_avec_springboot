22bc958aae2431945901f7ba2be01b9f8699bf20
monsieur486 <monsieur486@gmail.com>
2025-07-17 17:29:07 +0200

    add CoverageResponseDto

--- src/main/java/com/mr486/safetynet/dto/CoverageResponseDto.java
+++ src/main/java/com/mr486/safetynet/dto/CoverageResponseDto.java
@@ -3,21 +3,29 @@ package com.mr486.safetynet.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
-public class FireStationCoverageResponseDto {
-  private List<PersonCovered> persons;
+public class CoverageResponseDto {
+  private List<PersonCoveredDto> persons;
   private int adultCount;
   private int childCount;
 
   @Data
   @AllArgsConstructor
-  public static class PersonCovered {
+  public static class PersonCoveredDto {
     private String firstName;
     private String lastName;
     private String address;
     private String phone;
+  }
+
+  public void addPerson(PersonCoveredDto person) {
+    if (persons == null) {
+      persons = new ArrayList<>();
+    }
+    persons.add(person);
   }
 }
