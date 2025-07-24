e0649aead99d87e01d7bf9890fff638414668c11
monsieur486 <monsieur486@gmail.com>
2025-07-24 16:42:14 +0200

    add javadoc

--- src/main/java/com/mr486/safetynet/controller/MedicalRecordController.java
+++ src/main/java/com/mr486/safetynet/controller/MedicalRecordController.java
@@ -7,6 +7,10 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * MedicalRecordController handles HTTP requests related to medical records.
+ * It provides endpoints for creating, updating, deleting, and retrieving medical records.
+ */
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/medicalRecord")
@@ -14,11 +18,24 @@ public class MedicalRecordController {
 
   private final MedicalRecordService medicalRecordService;
 
+  /**
+   * Retrieves all medical records.
+   *
+   * @return an iterable of all MedicalRecord objects.
+   */
   @GetMapping(path = "/all", produces = "application/json")
   public Iterable<MedicalRecord> getAllMedicalRecords() {
     return medicalRecordService.findAll();
   }
 
+  /**
+   * Adds a new medical record for a person identified by firstName and lastName.
+   *
+   * @param firstName        the first name of the person.
+   * @param lastName         the last name of the person.
+   * @param medicalRecordDto the DTO containing medical record data.
+   * @return the saved MedicalRecord object.
+   */
   @PostMapping(path = "/firstname/{firstName}/lastname/{lastName}", produces = "application/json")
   public MedicalRecord addMedicalRecord(
           @PathVariable String firstName,
@@ -27,6 +44,14 @@ public class MedicalRecordController {
     return medicalRecordService.save(firstName, lastName, medicalRecordDto);
   }
 
+  /**
+   * Updates an existing medical record for a person identified by firstName and lastName.
+   *
+   * @param firstName        the first name of the person.
+   * @param lastName         the last name of the person.
+   * @param medicalRecordDto the DTO containing updated medical record data.
+   * @return the updated MedicalRecord object.
+   */
   @PutMapping(path = "/firstname/{firstName}/lastname/{lastName}", produces = "application/json")
   public MedicalRecord updateMedicalRecord(
           @PathVariable String firstName,
@@ -35,6 +60,12 @@ public class MedicalRecordController {
     return medicalRecordService.update(firstName, lastName, medicalRecordDto);
   }
 
+  /**
+   * Deletes a medical record for a person identified by firstName and lastName.
+   *
+   * @param firstName the first name of the person.
+   * @param lastName  the last name of the person.
+   */
   @DeleteMapping(path = "/firstname/{firstName}/lastname/{lastName}", produces = "application/json")
   public void deleteMedicalRecord(
           @PathVariable String firstName,
@@ -42,6 +73,13 @@ public class MedicalRecordController {
     medicalRecordService.delete(firstName, lastName);
   }
 
+  /**
+   * Retrieves a medical record by the first name and last name of the person.
+   *
+   * @param firstName the first name of the person.
+   * @param lastName  the last name of the person.
+   * @return the MedicalRecord object if found, or null if not found.
+   */
   @GetMapping(path = "/firstname/{firstName}/lastname/{lastName}", produces = "application/json")
   public MedicalRecord getMedicalRecordByFirstNameAndLastName(
           @PathVariable String firstName,
